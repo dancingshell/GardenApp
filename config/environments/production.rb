641e414bfaed039c63cb14006866a501b099f322
dancingshell <kaitlinjaffe@gmail.com>
2014-08-15 14:08:53 -0700

    edit production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ GardenApp2::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
