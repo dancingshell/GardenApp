de1b1b9348e426b9dc15a48523ac20f133d87f04
dancingshell <kaitlinjaffe@gmail.com>
2014-08-16 22:48:11 -0700

    changed user model to correctly accept zipcode working with heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,12 @@ class User
   field :last_name, type: String
   field :password_digest, type: String  
   field :email, type: String
+  field :zipcode, type: String
 
   has_secure_password
   has_mongoid_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, presence: true
-  # validates_length_of :password, minimum: 8, too_short: 'way too short', too_long: 'way to long'
-  # validates_confirmation_of :password
-  # validates_uniqueness_of :email
 
   validates_presence_of :email, :password, :password_confirmation
   validates :password, length: { in: 8..12 }
@@ -25,6 +20,6 @@ class User
   validates_uniqueness_of :email
 
 
-  belongs_to :zipcode
+  # belongs_to :zipcode
   has_many :gardens
 end
